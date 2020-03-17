3e92cb376a2c77884277a1075af180429b70fbf0
Abhishek Thakur <kumarcops@gmail.com>
2020-03-18 02:42:00 +0530

    Comp

---
+++ index.js
@@ -0,0 +1,13 @@
+
+ 
+ var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    strings : [
+        'Full Stack Developer',
+        'Freelancer'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+  });
+
+ 
